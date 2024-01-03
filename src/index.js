501ac0d6eff233bbfa61a46a9ef91e0fc72a5b4e
Roman Shuvalov <romanshuvalov@yahoo.com>
2024-01-03 22:19:36 +0200

    chore: add space to response

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ const http = require('http');
 http
   .createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(`Version: ${process.env.APP_VERSION}`);
+    res.end(`Version: ${process.env.APP_VERSION}\n`);
   })
   .listen(3000, () => console.log('Server running on port 3000'));
